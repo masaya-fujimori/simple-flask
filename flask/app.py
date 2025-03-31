af260157742fea7e26e69db5ba85566c480cb598
masaya-fujimori <masaya_fujimori@densan-s.co.jp>
2025-03-31 17:52:07 +0900

    すべてのオリジンを許可

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # フロントエンドからのリクエストを許可
+CORS(app, resources={r"/api/*": {"origins": "*"}})  # フロントエンドからのリクエストを許可（すべてのオリジンを許可）
 
 @app.route('/api/greet')
 def greet():
